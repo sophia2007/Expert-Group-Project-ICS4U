bfd3d5bfc8487f603eeb387d6da44fc3686745ea
sophia2007 <62575423+sophia2007@users.noreply.github.com>
2024-10-06 14:21:13 -0400

    Add files via upload
    Updated code with features like metronome and button.

--- script.js
+++ script.js
@@ -1,16 +1,64 @@
 
 window.addEventListener('keydown', function () {
-    const audio = document.querySelector(`audio[data-key="${event.key.toUpperCase()}"]`);
-    const key = document.querySelector(`.key[data-key="${event.key.toUpperCase()}"]`);
+    play(event.key.toUpperCase());
+});
+
+var button = document.getElementById("song1").addEventListener("click", playSong1);
+
+function playSong1() {
+    // Q  2  W  3  E  4  R  T  6  Y  7  U  I  9  O  0  P  -  [  ]
+    // F F#  G  G# A  A# B  C  C# D  D# E  F  F# G  G# A  A# B  C
+    const notes1 = [
+        "R", "U", "O", "R", "U", "O", "R", "U", "O", "R", "U", "O",
+        "R", "U", "O", "R", "U", "O", "R", "U", "O", "R", "U", "O",
+        "T", "U", "O", "T", "U", "O", "T", "I", "P", "T", "I", "P",
+        "R", "7", "P", "R", "U", "O", "R", "U", "9", "T", "7", "9",
+    ]
+
+    const notes = [
+        "T", "U", "O"
+    ]
+
+    for (i = 0; i < notes.length; i++) {
+        play(notes[i]);
+    }
+
+}
+
+function play(key) {
+    const audio = document.querySelector(`audio[data-key="${key}"]`);
+    const keyElement = document.querySelector(`.key[data-key="${key}"]`);
     if (!audio) return;
 
-    key.classList.add('active');
+    keyElement.classList.add('active');
     audio.currentTime = 0;
     audio.play();
 
     setTimeout(() => {
-        key.classList.remove('active');
-    }, 100);
+        keyElement.classList.remove('active');
+    }, 200);
+}
+
+var metAudio = new Audio('/audio/met.wav');
+var slider = document.getElementById("myRange");
+var output = document.getElementById("value");
+var checkbox = document.querySelector('input[type="checkbox"]');
 
 
+slider.oninput = function () {
+    output.innerHTML = this.value;
+}
+
+checkbox.addEventListener('change', function () {
+    if (checkbox.checked) {
+        MetroInterval = setInterval(function () {
+            metAudio.currentTime = 0;
+            metAudio.play();
+        }, ((60 / slider.value) * 1000));
+    } else {
+        clearInterval(MetroInterval);
+    }
 });
+
+
+
